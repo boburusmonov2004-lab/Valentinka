3ce3d5462603b0ea9d357d80ce724ced46fc3c42
Bobur Usmonov <boburusmonov@Boburs-MacBook-Air.local>
2026-02-10 00:00:39 +0500

    Updated photos

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -9,7 +10,37 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        ViteImageOptimizer({
+          test: /\.(jpe?g|png|gif|tiff?|webp|svg)$/i,
+          exclude: undefined,
+          include: undefined,
+          includePublic: true,
+          logStats: true,
+          ansiColors: true,
+          sharp: {
+            encodeFormat: 'webp',
+            encodeOptions: {
+              webp: { quality: 75 },
+            },
+          },
+          svg: {
+            multipass: true,
+            plugins: [
+              {
+                name: 'removeViewBox',
+              },
+              {
+                name: 'removeEmptyNS',
+              },
+              {
+                name: 'cleanupNumericValues',
+              },
+            ],
+          },
+        }),
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
